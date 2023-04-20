12bb6117773425af59e851d16362da8e4f41f3a9
Hallow87 <emilgranath05@gmail.com>
2023-04-20 13:25:56 +0300

    updt

--- a28/js/a1.js
+++ a28/js/a1.js
@@ -31,3 +31,36 @@ bgColor.addEventListener('keyup', function(event) {
          break
  }
 });
+
+
+ // Defining event listener function
+function displayWindowSize() {
+  var w = window.innerWidth;
+  var h = window.innerHeight;
+  var size = "Width: " + w + ", " + "Height: " + h;
+  document.getElementById("windowSize").innerHTML = size;
+}
+
+// Event listener looking for window resize
+window.addEventListener("resize", displayWindowSize);
+
+
+// Button that changes when it's pressed
+var btn = document.getElementById("myBtn");
+ 
+function sayHello() {
+    alert("Look at the button again");
+}
+ 
+function setHoverColor() {
+    btn.style.background = "yellow";
+}
+ 
+function setNormalColor() {
+    btn.style.background = "";
+}
+ 
+// Assigning event listeners to the button
+btn.addEventListener("click", sayHello);
+btn.addEventListener("mouseover", setHoverColor);
+btn.addEventListener("mouseout", setNormalColor);
