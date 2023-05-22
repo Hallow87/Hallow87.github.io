d9ee59d2eb69bbb767d68a63db76ec534bf88a30
Hallow87 <emilgranath05@gmail.com>
2023-05-22 09:22:13 +0300

    Jenni sekottaa

---
+++ JavaScript/a43/js/a1.js
@@ -0,0 +1,18 @@
+
+
+
+const arr = ['javascript', 'python', 'php']
+
+arr.forEach(languages => {
+   console.log(languages)
+});
+
+//greet = (name, place) => 'Hello ${name}. Welcome to ${place}'
+
+//const hi = greet ('henry', 'canada')
+//console.log (hi)
+
+//function greet (name, place) {
+//   this.name = name
+//   console.log (name
+
