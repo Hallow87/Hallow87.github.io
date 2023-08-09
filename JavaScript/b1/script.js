8ff8782c3f15a91b137d6d6053a969dfc2f86a54
Emil <emilgranath05@gmail.com>
2023-08-09 11:07:56 +0300

    update

--- JavaScript/b1/script.js
+++ JavaScript/b1/script.js
@@ -3,6 +3,11 @@ const numberOfSquares = 16;
 var i = 0;
 let square1, square2;
 let clickCount = 0;
+let score = 0;
+document.querySelector("#score").style.visibility = "hidden";
+const playAgainBtn = document.querySelector('button')
+playAgainBtn.style.visibility = "hidden"
+playAgainBtn.addEventListener('click', playAgain)
 let colors = [
     "#232a51",
     "#232a51",
@@ -20,8 +25,6 @@ let colors = [
     "#7169ad",
     "#958ec1",
     "#958ec1",
-    "#b8b4d6",
-    "#b8b4d6",
 ];
 function selectColor() {
     const random = Math.floor(Math.random() * colors.length);
@@ -45,15 +48,61 @@ for (const square of squares) {
 }
 
 function squareClicked() {
+    if(square1 == this) return;
     clickCount++;
-    clickCount === 1 ? (square1 = this) : (square2 = this)
+    if (clickCount > 2) return;
+    clickCount === 1 ? (square1 = this) : (square2 = this);
     if(clickCount === 1) {
         square1.style.background = square1.getAttribute("data-color")
     } else {
-        square2.style.background = square2.getAttribute("data-color")
+        square2.style.background = square2.getAttribute("data-color");
+        checkMatch();
     }
 }
+function checkMatch() {
+    let match =
+        square1.getAttribute('data-color') === square2.getAttribute('data-color')
+        if(!match){
+            setTimeout(function(){
+                noMatch();
+            }, 500);
+        } else {
+            isMatch();
+            checkGameEnded();
+        }
+}
 
+function noMatch() {
+    square1.style.background = "lavender";
+    square2.style.background = "lavender";
+    square1 = "";
+    square2 = "";
+    clickCount = 0;
+    console.log("no match")
+
+}
+
+function isMatch() {
+    score++
+    document.querySelector("#score").innerText = score;
+    document.querySelector("#score").style.visibility = "visible";
+    square1.style.border = "none";
+    square2.style.border = "none";
+    square1.removeEventListener("click", squareClicked)
+    square2.removeEventListener("click", squareClicked)
+    clickCount = 0;
+    console.log("it's a match")
+}
+function checkGameEnded(){
+    const target = numberOfSquares / 2;
+    const gameOver = score === target ? true : false;
+    if(gameOver) {
+        playAgainBtn.style.visibility = "visible"
+    }
+}
+function playAgain() {
+    window.location.reload()
+}
 //selectColor();
 //selectColor();
 //console.log(selectColor());
